cc14c1e6881d056d9f387a682961d80402219ac8
Romeu Lima <romeu.sousadp@gmail.com>
2024-10-29 14:37:20 -0300

    updates

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, type OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) { }
+  
+  formGroup!: FormGroup
+  results = []
+  
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
 
+    
+  }
+
+  onSubmit(): void {
+    console.log(this.formGroup.value)
+  }
+
+  onClear(): void {
+    this.formGroup.reset()
+  }
 }
